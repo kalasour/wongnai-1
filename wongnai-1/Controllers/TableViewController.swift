fcb36e45e4e0398adb1257419cb3ffa9061164f3
kalasour <nuntawat_wisedsup@cmu.ac.th>
2019-11-11 01:13:53 +0700

    have insertCell by given image

--- wongnai-1/Controllers/TableViewController.swift
+++ wongnai-1/Controllers/TableViewController.swift
@@ -10,7 +10,8 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
-    private var gotData: Item?
+    private var gotData:Item?
+    private var insertEvery:Int = 5
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -37,20 +38,27 @@ class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if let unwrapData = gotData {
-            return unwrapData.getPhotos().count
+            return (unwrapData.getPhotos().count+(unwrapData.getPhotos().count/(insertEvery-1)))-1
         }else {
             return 0;
         }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell") as? ItemTableViewCell else { return UITableViewCell()
+        let i:Int = indexPath.row + 1
+        if(i % insertEvery != 0){
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell") as? ItemTableViewCell else { return UITableViewCell()
+            }
+            
+            if let unwrapData = gotData {
+                let calIndex = (i-(i/insertEvery))-1
+                cell.setProps(photo: unwrapData.getPhotos()[calIndex])
+            }
+            return cell
+        }else {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "InsertCell") else { return UITableViewCell() }
+            return cell
         }
-        
-        if let unwrapData = gotData {
-            cell.setProps(photo: unwrapData.getPhotos()[indexPath.row])
-        }
-        return cell
     }
     
     override func tableView(_ tableView: UITableView,willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
